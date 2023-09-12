004ae5bdb0a25dc19e756c16ff58156f186b3ab3
Prashanna Lohani <plohani2003@gmail.com>
2023-09-12 12:16:01 +0545

    Detail page Complete

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,26 +1,95 @@
-import { Box, Button, Flex, List, ListItem, Spacer } from "@chakra-ui/react";
+import {
+  Heading,
+  Image,
+  Flex,
+  Box,
+  List,
+  ListItem,
+  Button,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
 export default function Description() {
-  const listKey = [
-    "Name:",
-    "Availability:",
-    "Description:",
-    "Discount:",
-    "Quantity:",
-  ];
+  const [value, setValue] = useState(0);
+
+  function handleAddition() {
+    setValue(value + 1);
+  }
+  function handleSub() {
+    if (value < 0 || value == 0) {
+      return setValue(0);
+    } else {
+      return setValue(value - 1);
+    }
+  }
+
   return (
     <>
-      <Flex align="center" f>
-        <List fontSize="2xl">
-          {listKey.map((item, index) => (
-            <ListItem key={index} mt={`${index * 1}rem`}>
-              {item}
-            </ListItem>
-          ))}
+      <Box>
+        <Heading as="h1" ml="1rem">
+          Dummy Name
+        </Heading>
+        <Image
+          objectFit="cover"
+          src="https://img.freepik.com/premium-photo/food-photography_841014-8181.jpg"
+          maxH={{ base: "20rem", lg: "30rem", sm: "20rem" }}
+          borderRadius="2rem"
+          mt="2rem"
+        />
+      </Box>
+      <Box minH="30rem" p="2rem 3rem" display="flex" alignItems="center">
+        <List fontSize="2rem">
+          <Flex flexDir="column" gap="2rem" mt="2rem">
+            <ListItem>Name:</ListItem>
+            <ListItem>Price:</ListItem>
+            <ListItem>Description:</ListItem>
+            <ListItem>Quantity:</ListItem>
+          </Flex>
         </List>
-        <Button colorScheme="orange" mt="2rem">
-          Confirm
-        </Button>
-      </Flex>
+      </Box>
+      <Box
+        minH="30rem"
+        p="2rem 3rem"
+        display="flex"
+        alignItems="center"
+        position="relative"
+      >
+        <List fontSize="2rem" w="15rem">
+          <Flex flexDir="column" gap="2rem" mt="2rem">
+            <ListItem>Dummy Name</ListItem>
+            <ListItem>Dummy Price</ListItem>
+            <ListItem>Dummy Name</ListItem>
+            <ListItem>
+              <Flex align="center" gap="2px">
+                <Button
+                  fontSize="2rem"
+                  colorScheme="whiteAlpha"
+                  onClick={handleSub}
+                >
+                  -
+                </Button>
+                <Input
+                  type="number"
+                  maxW="4rem"
+                  value={value}
+                  disabled
+                  textAlign="center"
+                />
+                <Button
+                  fontSize="2rem"
+                  colorScheme="whiteAlpha"
+                  onClick={handleAddition}
+                >
+                  +
+                </Button>
+              </Flex>
+            </ListItem>
+          </Flex>
+          <Button colorScheme="orange" position="absolute" mt="2rem" ml="7rem">
+            Confirm
+          </Button>
+        </List>
+      </Box>
     </>
   );
 }
--- src/Page/Detail.jsx
+++ src/Page/Detail.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, SimpleGrid } from "@chakra-ui/react";
 import Description from "../Components/Description";
 
 export default function Detail() {
@@ -7,25 +7,15 @@ export default function Detail() {
       <Box bg="#111" minH="100vh" p="2rem 3rem">
         <Box
           bg="#1f1f1f"
-          minH="100vh"
+          minH="auto"
           borderRadius="2rem"
           p="3rem 7rem "
           color="whiteAlpha.700"
-          position="relative"
+          mt="5rem"
         >
-          <Heading as="h1" ml="1rem">
-            Dummy Name
-          </Heading>
-          <Flex columnGap="4rem">
-            <Image
-              objectFit="cover"
-              src="https://img.freepik.com/premium-photo/food-photography_841014-8181.jpg"
-              maxH={{ base: "20rem", lg: "30rem", sm: "20rem" }}
-              borderRadius="2rem"
-              mt="3rem"
-            />
+          <SimpleGrid templateColumns="repeat(3,1fr)" spacing="0.5em">
             <Description />
-          </Flex>
+          </SimpleGrid>
         </Box>
       </Box>
     </>
